15c74f39cae7e5d01bdcd478c482c0677a2aab9b
HieuPham-origin <hieupham112003@gmail.com>
2024-03-28 00:22:45 +0700

    create proj

---
+++ booking/src/main/java/vn/edu/tdtu/booking/models/Seat.java
@@ -0,0 +1,4 @@
+package vn.edu.tdtu.booking.models;
+
+public class Seat {
+}
---
+++ booking/src/test/java/vn/edu/tdtu/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.tdtu.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
